5638dc6b857c79885f3735330fab41f4e508f5a4
Diego Hernandez <dhdezr@fpgaparadox.com>
2021-04-09 10:45:00 -0500

    Adding script to resize and clone

--- virtualbox/U1-resize_and_clone.sh
+++ virtualbox/U1-resize_and_clone.sh
@@ -2,16 +2,22 @@
 export STORE_PATH=${PWD}/zero_to_asic_vm
 export VM_NAME=ubuntu
 export OLD_NAME=ubuntu
-export NEW_NAME=ubuntu_resized
+export NEW_NAME=ubuntu_40
 export HDD_SIZE=40
+
+# Create new HDD
+echo "[- Message -] Creating an HDD of size $HDD_SIZE GB stored in $STORE_PATH/$NEW_NAME.vdi"
+VBoxManage createmedium disk --filename "$STORE_PATH/$NEW_NAME.vdi" --size $((HDD_SIZE*1024)) --format VDI --variant Standard
 
 # Clone the old hdd into this newly created VDI
 echo "[- Message -] Cloning old HDD $STORE_PATH/$OLD_NAME.vdi to $STORE_PATH/$NEW_NAME.vdi"
-VBoxManage clonehd $STORE_PATH/$OLD_NAME.vdi $STORE_PATH/$NEW_NAME.vdi --variant Standard
+VBoxManage clonemedium $STORE_PATH/$OLD_NAME.vdi $STORE_PATH/$NEW_NAME.vdi --variant Standard --existing
+
 # Attach new HDD to the VM
 # Disconnect old HDD
 echo "[- Message -] Disconnecting old HDD"
 VBoxManage storageattach $VM_NAME --storagectl SATA --port 0 --type hdd --medium none
+
 # Connect the HDD to the created VM
 echo "[- Message -] Attaching the virtual HDD $STORE_PATH/$NEW_NAME.vdi to the VM $NAME"
 VBoxManage storageattach $VM_NAME --storagectl SATA --port 0 --type hdd --medium $STORE_PATH/$NEW_NAME.vdi
